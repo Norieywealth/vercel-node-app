8814217d7a614cd2392b8be257a3745f905efa2b
Nnamdi Orie <nnamdi.orie@iisysgroup.com>
2024-09-17 13:35:36 +0100

    vercel config

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cors())
-const port = 3000
+
+app.get("/testing", async(req,res) => {
+    res.send(true)
+})
+
+const port = process.env.PORT || 3001
 app.listen(port, () => console.log('Vercel app is listening on port ' + port))
